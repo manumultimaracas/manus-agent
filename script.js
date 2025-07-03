9011d596ccb38325cbf6cd8eaa47a5ca01523657
manumultimaracas <lojamanumultimarcas@gmail.com>
2025-07-03 15:41:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-console.log("Site Manu ativo!");
-document.querySelector(".next").onclick = () => alert("Próximo slide");
-document.querySelector(".prev").onclick = () => alert("Slide anterior");
+console.log('Site carregado.');
